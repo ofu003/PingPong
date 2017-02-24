0a2c9eb75b450978974b294f59e3c4349e29ab40
oliver fu <ofu997@gmail.com>
2017-02-24 10:15:31 -0800

    Expand functions in scripts.js to satisfy requirement for displaying Pong for multiples of 5 and Ping-Pong for multiples of 15

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
+//back-end function
 var PingPong=function(userInput){
-  if (userInput%3==0){
+  if (userInput%15==0){
+    $(".outputDisplay").text("Ping-Pong");
+  }
+  else if (userInput%5==0){
+    $(".outputDisplay").text("Pong");
+  }
+  else if (userInput%3==0){
     $(".outputDisplay").text("Ping");
   }
   else{
     $(".outputDisplay").text(userInput);
   }
-
 }
-
-
+//front-end functions
 $(function(){
   $("form#form").submit(function(event){
    event.preventDefault();
