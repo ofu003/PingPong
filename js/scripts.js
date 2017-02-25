96ec5086cc9035fe2a5a08200c373ee7d1cc0086
oliver fu <ofu997@gmail.com>
2017-02-24 16:12:57 -0800

    Add to styles.css and use bootstrap in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,45 @@
-//back-end function
-
-var PingPong=function(userInput){
+//back-end
+$(function(){
+  var PingPong=function(userInput){
   arr = [];
   for (var i=1; i<=userInput; i++){
   arr.push(i);
   console.log(arr);
   }
 
-  arr.forEach(function(i){
-    if (i%15==0){
-      $(".outputDisplay").show();
-      $(".outputDisplay").append("<li>" + "Ping-Pong" + "</li>");
-    }
-    else if (i%5==0){
-      $(".outputDisplay").show();
-      $(".outputDisplay").append("<li>" + "Pong" + "</li>");
-    }
-    else if (i%3==0){
-      $(".outputDisplay").show();
-      $(".outputDisplay").append("<li>" + "Ping" + "</li>");
-    }
-    else{
-      $(".outputDisplay").show();
-      $(".outputDisplay").append("<li>" + i + "</li>");
-    }
-  })
-}
+    arr.forEach(function(i){
+      if (i%15==0){
+        $(".outputDisplay").show();
+        $(".outputDisplay").append("<li>" + "Ping-Pong" + "</li>");
+      }
+      else if (i%5==0){
+        $(".outputDisplay").show();
+        $(".outputDisplay").append("<li>" + "Pong" + "</li>");
+      }
+      else if (i%3==0){
+        $(".outputDisplay").show();
+        $(".outputDisplay").append("<li>" + "Ping" + "</li>");
+      }
+      else{
+        $(".outputDisplay").show();
+        $(".outputDisplay").append("<li>" + i + "</li>");
+      }
+    })
+  }
 //front-end functions
-$(function(){
   $("form#form").submit(function(event){
-   event.preventDefault();
+    event.preventDefault();
 
-   var userInput=$("#input").val();
+    var userInput=$("#input").val();
 
-   PingPong(userInput);
+    PingPong(userInput);
 
-   $("#reset").show();
+    $("#reset").show();
 
     $("#reset").click(function(){
       userInput="";
       arr=[];
       $(".outputDisplay").text(arr);
-
-      //$(".outputDisplay").hide();
     })
   })
-})
+});
