cfe871475598190bff80e9cc3d684006385d3ce7
Oliver Fu <ofu997@gmail.com>
2017-03-08 02:51:39 -0800

    Edit comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-//declare universal variable to store numbers
+//Declares universal variable to store numbers
 var arrayOfNumbers = [];
 //PingPong function populates arrayOfNumbers and assigns string values to numbers
 var PingPong=function(userInput){
-  //push numbers from 1 up to userInput value into //arrayOfNumbers
+  //Pushes numbers from 1 up to userInput value into //arrayOfNumbers
   for (var i=1; i<=userInput; i++){
   arrayOfNumbers.push(i);
   }
-  //assigns string values to numbers
+  //Assigns string values to numbers
   arrayOfNumbers.forEach(function(number){
     if (number%15==0){
       arrayOfNumbers[(number-1)]="Ping-Pong";
@@ -25,27 +25,27 @@ var PingPong=function(userInput){
 
 
 $(function(){
-  //submit stores input, calls PingPong function, displays results,
+  //Submit function stores input, calls PingPong function, and displays results
   $("form#form").submit(function(event){
     event.preventDefault();
-    //store input as variable
+    //Stores input as variable
     var userInput=$("#input").val();
-    //call PingPong function
+    //Calls PingPong function
     PingPong(userInput);
 
-    //display arrayOfNumbers after it has passed through PingPong function
+    //Displays arrayOfNumbers after it has passed through PingPong function
     arrayOfNumbers.forEach(function(number){
     $(".outputDisplay").append("<li>"+number+"</li>");
     $("#outputBox").show();
     $("#reset").show();
     });
   });
-    //When reset button is pressed, the below happens
+    //Reset function clears page 
     $("#reset").click(function(){
       userInput="";
       arrayOfNumbers=[];
-      $(".outputDisplay").text(arrayOfNumbers);
       $("#reset").hide();
+      $(".outputDisplay").text(arrayOfNumbers);
       $("#outputBox").hide();
     })
 });
